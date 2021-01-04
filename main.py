af8587353b582b10155164f0c114ad39ae228c52
bignellrp <bignellrp@gmail.com>
2021-01-04 09:54:09 +0000

    Fixed the heap code using an int

--- main.py
+++ main.py
@@ -13,6 +13,16 @@ from flask import Flask, render_template, request
 #from player import all_players, player_names
 #import google_sheets
 
+def even_teams(game_players, n=2):
+    teams = [[] for _ in range(n)]
+    totals = [(0, i) for i in range(n)]
+    heapq.heapify(totals)
+    for obj in game_players:
+        total, index = heapq.heappop(totals)
+        teams[index].append(obj)
+        heapq.heappush(totals, (total + obj[1], index))
+    return tuple(teams)
+
 app = Flask(__name__)
 
 # If modifying these scopes, delete the file token.pickle.
@@ -69,23 +79,26 @@ def index():
         game_players = []
         for obj in all_players: 
             if obj.name in available_players:
-                game_players.append((obj.name , obj.score))
+                game_players.append((obj.name , int(obj.score)))
         # Sort game_players by the key of the second column
-        game_players.sort(key=lambda x:x[1])
+        #game_players.sort(key=lambda x:x[1])
         # Put the Even elements from the array starting from zero
         # and counting in twos into Team A
-        team_a = game_players[0::2]
+        #team_a = game_players[0::2]
         # Put the Odd elements from the array starting from 1 
         # and counting in twos into Team B
-        team_b = game_players[1::2]
+        #team_b = game_players[1::2]
+
+        team_a, team_b = even_teams(game_players)
+
         # Take the first column and put names into team_a and team_b
         team_a_names = ([row[0] for row in team_a])
         team_b_names = ([row[0] for row in team_b])
         # Calculate the score
         team_a_score = ([row[1] for row in team_a])
         team_b_score = ([row[1] for row in team_b])
-        team_a_score = [int(i) for i in team_a_score]
-        team_b_score = [int(i) for i in team_b_score]
+        #team_a_score = [int(i) for i in team_a_score]
+        #team_b_score = [int(i) for i in team_b_score]
         # Sum the total of the scores
         team_a_total = (sum(team_a_score))
         team_b_total = (sum(team_b_score))
