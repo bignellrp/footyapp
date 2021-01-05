4ead377ad764d625ec277acc457801fb1090d279
Richard Bignell <bignellrp@gmail.com>
2021-01-05 19:49:58 +0000

    Added googlesheet function to a separate file

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from __future__ import print_function
-import pickle, os.path, sys, heapq
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import os.path, sys, heapq
+from googlesheet import *
 from json_date import next_wednesday
 from flask import Flask, render_template, request
 sys.dont_write_bytecode = True
@@ -19,49 +16,11 @@ def even_teams(game_players, n=2):
 
 app = Flask(__name__)
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-
-# The ID and range of a sample spreadsheet.
-SPREADSHEET_ID = '1tyy_8sKM-N-JA6j1pASCO6_HRxvlhTuA3R0KysbVG9U'
-RANGE_NAME = 'Sheet2!A2:G'
-WRITE_RANGE_NAME = 'Sheet3!A1:AA1000'
-
-creds = None
-
-if os.path.exists('token.pickle'):
-    with open('token.pickle', 'rb') as token:
-        creds = pickle.load(token)
-
-if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-# Save the credentials for the next run
-    with open('token.pickle', 'wb') as token:
-        pickle.dump(creds, token, protocol=2)
-service = build('sheets', 'v4', credentials=creds)
-
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
-                                range=RANGE_NAME).execute()
-values = result.get('values', [])
-player_names = ([row[0] for row in values])
-all_players = []
-class Player:
-    def __init__(self, name, score):
-        self.name = name
-        self.score = score
-for row in values:
-    all_players.append( Player( row[0], row[6] ))
 #Start the Web Form for pulling the checkbox data input
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    googlefunction()
     if request.method == 'POST':
-    
         # Use GetList to put the data from the index template into the array
         available_players = []
         available_players = request.form.getlist('available_players')
@@ -121,8 +80,8 @@ def index():
 #Start the Compare Web Form for pulling the checkbox data input
 @app.route('/compare', methods=['GET', 'POST'])
 def compare():
+    googlefunction()
     if request.method == 'POST': 
-
         # Use GetList to put the data from the index template into the array
         available_players_a = []
         available_players_a = request.form.getlist('available_players_a')
