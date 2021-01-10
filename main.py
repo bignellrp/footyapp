b5fe62b6d17d131883ec0df092ff87d5ca8f6405
Richard Bignell <bignellrp@gmail.com>
2021-01-10 19:47:16 +0000

    Fixed error in template with help from reddit

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from routes.compare import compare_blueprint
 from routes.index import index_blueprint
 
 app = Flask(__name__)
-
-app.register_blueprint(compare_blueprint, url_prefix='/compare')
-app.register_blueprint(index_blueprint, url_prefix='/')
+# Register the blueprint for each route
+app.register_blueprint(index_blueprint)
+app.register_blueprint(compare_blueprint)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", debug=True, port=5000)
