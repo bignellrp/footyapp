9a343e2f238bc766ef529a6820e6de5f756132c5
bignellrp <bignellrp@gmail.com>
2021-01-15 19:28:21 +0000

    Updated Static to match folder structure from main

--- routes/index.py
+++ routes/index.py
@@ -1,35 +1,31 @@
 from flask import Flask, render_template, request, Blueprint
-from services.json_date import next_wednesday
-from services.getplayers import _make_players, _fetch_sheet, SERVICE, SCOPES, SPREADSHEET_ID, WRITE_RANGE_NAME, RANGE_NAME
-from google.auth.transport.requests import Request
+from services.player import all_players, player_names
 from services.teams import even_teams
 
 index_blueprint = Blueprint('index', __name__, template_folder='templates', static_folder='static')
 
 @index_blueprint.route('/', methods=['GET', 'POST'])
 def index():
-    """Start the Web Form for pulling the checkbox data input"""
-    all_players, player_names = _make_players(_fetch_sheet())
-    if request.method == 'POST':
-
+    if request.method == 'POST': 
+    
         # Use GetList to put the data from the index template into the array
         available_players = []
         available_players = request.form.getlist('available_players')
-
+        
         # Define game_players array for storing the players names 
         # and scores only if the names are listed in available_players
         game_players = []
-        for obj in all_players:
+        for obj in all_players: 
             if obj.name in available_players:
                 game_players.append((obj.name , int(obj.score)))
-
-        # Add the even teams into team a and team b
+        
+        # Take the even teams elements from the heap function
         team_a, team_b = even_teams(game_players)
 
         # Take the first column and put names into team_a and team_b
         team_a_names = ([row[0] for row in team_a])
         team_b_names = ([row[0] for row in team_b])
-
+        
         # Calculate the score
         team_a_score = ([row[1] for row in team_a])
         team_b_score = ([row[1] for row in team_b])
@@ -38,32 +34,6 @@ def index():
         team_a_total = (sum(team_a_score))
         team_b_total = (sum(team_b_score))
 
-        # Post Results to Google Sheets if output is checked
-        output_checked = []
-        output_checked = request.form.getlist('output_checked')
-        if output_checked == ['Yes']:
-            # Format the google ouput into one long comma sep str
-            google_output = []
-            google_output.append((next_wednesday))
-            google_output.append(("TeamAResult?"))
-            google_output.append(("TeamBResult?"))
-            google_output.append((team_a_total))
-            google_output.append((team_b_total))
-            for obj in team_a_names: 
-                google_output.append((obj))
-            for obj in team_b_names: 
-                google_output.append((obj))
-            # Format the google body for ROWS
-            body = {
-                'majorDimension': 'ROWS',
-                'values': [
-                    google_output,
-                ],
-                }
-            # Print the result to google sheets with append enabled
-            result = SERVICE.spreadsheets().values().append(
-                spreadsheetId=SPREADSHEET_ID, range=WRITE_RANGE_NAME,
-                valueInputOption='USER_ENTERED', body=body).execute()
         # Return Team A and Team B to the results template
         return render_template('result.html', teama = team_a_names, teamb = team_b_names, scorea = team_a_total, scoreb = team_b_total)
-    return render_template('index.html', player_names=player_names)
+    return render_template('index.html', player_names = player_names)
---
+++ services/player.py
@@ -0,0 +1,39 @@
+# Define the Player Structure into Class Player
+class Player:
+    def __init__(self, number, name, passing, stamina, goalkeeping, defense, offense):
+        self.number = number
+        self.name = name
+        self.passing = passing
+        self.stamina = stamina
+        self.goalkeeping = goalkeeping
+        self.defense = defense
+        self.offense = offense
+        self.score = passing + stamina + goalkeeping + defense + offense
+
+# Enter the Player Data into an array called all_players
+all_players = []
+all_players.append( Player(1,'Chris',17,17,12,18,15) )
+all_players.append( Player(2,'Gavin',19,18,14,17,19) )
+all_players.append( Player(3,'Pete',16,14,13,16,19) )
+all_players.append( Player(4,'Kevin',16,11,12,15,17) )
+all_players.append( Player(5,'Ferdi',16,11,10,16,18) )
+all_players.append( Player(6,'Cal',14,11,10,12,9) )
+all_players.append( Player(7,'Tom',12,9,10,11,10) )
+all_players.append( Player(8,'Joe',14,15,18,17,18) )
+all_players.append( Player(9,'Simon',12,15,19,14,9) )
+all_players.append( Player(10,'Rik',12,19,10,14,12) )
+all_players.append( Player(11,'James',14,17,11,11,15) )
+all_players.append( Player(12,'Phil',18,17,11,15,18) )
+all_players.append( Player(13,'Dida',17,15,20,15,19) )
+all_players.append( Player(14,'Rich',17,17,18,15,19) )
+all_players.append( Player(15,'Bernard',16,15,16,13,9) )
+all_players.append( Player(16,'Matt',11,11,9,9,7) )
+all_players.append( Player(17,'Will',9,9,13,9,7) )
+all_players.append( Player(18,'Level1',11,11,9,9,7) )
+all_players.append( Player(19,'Level2',14,11,10,12,9) )
+all_players.append( Player(20,'Level3',16,11,12,15,17) )
+all_players.append( Player(21,'Level4',19,18,14,17,19) )
+
+player_names = []
+for obj in all_players: 
+    player_names.append((obj.name))
