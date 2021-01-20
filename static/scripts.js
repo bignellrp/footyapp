a9b8b244e45c28d78702414bfc578cdce05ce1df
bignellrp <bignellrp@gmail.com>
2021-01-20 23:23:48 +0000

    Cant get validation to work so posted on reddit

--- static/scripts.js
+++ static/scripts.js
@@ -56,25 +56,11 @@
     
     // Check number of checkboxes
 
-    $('#validcheckid').click(function(){
-        var checkCount = $("input[name='available_players']:checked").length;
-
-        if(checkCount == 10){
-        }
-        else {
-            alert('Please choose ten players.');
-        }
-    });
-
-    $('#validcheckidcompare').click(function(){
-        var checkCount_a = $("input[name='available_players_a']:checked").length;
-        var checkCount_b = $("input[name='available_players_b']:checked").length;
-
-        if((checkCount_a == 5) || (checkCount_b == 5)){
-        }
-        else {
-            alert('Please choose 5 players per team.');
-        }
+    var limit = 10;
+    $('input.single-checkbox').on('change', function(evt) {
+       if($(this).siblings(':checked').length >= limit) {
+           this.checked = false;
+       }
     });
 
 })(jQuery); // End of use strict
