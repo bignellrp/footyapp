b9e1c80314d0439e7eebd9cfa66dd747ec9a7a5e
Richard Bignell <bignellrp@gmail.com>
2021-05-20 00:02:05 +0100

    Post to google still broken

--- routes/result.py
+++ routes/result.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, Blueprint
 from services.json_date import next_wednesday
 from services.getplayers import sheet, SPREADSHEET_ID, STATS_TABLE_WRITE
-#from array import *
 
 result_blueprint = Blueprint('result', __name__, template_folder='templates', static_folder='static')
 
@@ -13,33 +12,35 @@ def result():
         teamb_passback = request.form.getlist('teamb_passback')
         scorea_passback = request.form.getlist('scorea_passback')
         scoreb_passback = request.form.getlist('scoreb_passback')
-        # Post Results to Google Sheets if output is checked
-        output_checked = []
-        output_checked = request.form.getlist('output_checked')
-        if output_checked == ['Yes']:
-            # Format the google ouput into one long comma sep str
-            google_output = []
-            google_output.append((next_wednesday))
-            google_output.append((0))
-            google_output.append((0))
-            google_output.append((scorea_passback))
-            google_output.append((scoreb_passback))
-            for obj in teama_passback: 
-                google_output.append((obj))
-            for obj in teamb_passback: 
-                google_output.append((obj))
-            print(google_output)
-            # Format the google body for ROWS
-            body = {
-                'majorDimension': 'ROWS',
-                'values': [
-                    google_output,
-                ],
-                }
-            # Print the result to google sheets with append enabled
-            result = sheet.values().update(
-                spreadsheetId=SPREADSHEET_ID, range=STATS_TABLE_WRITE,
-                valueInputOption='USER_ENTERED', body=body).execute()
+        google_output = []
+        google_output.append((next_wednesday))
+        google_output.append((0))
+        google_output.append((0))
+        google_output.append((scorea_passback))
+        google_output.append((scoreb_passback))
+        google_output.append((teama_passback))
+        google_output.append((teamb_passback))
+        print(google_output)
+        # for obj in teama_passback: 
+        #     google_output.append((obj))
+        # for obj in teamb_passback: 
+        #     google_output.append((obj))
+        # google_output = str(next_wednesday)[1:-1] + ',' + '0' + ',' + '0' + ',' + str(scorea_passback)[1:-1] + ',' + str(scoreb_passback)[1:-1] + ',' + teama_passback + ',' + teamb_passback
+        # str(google_output).translate(str.maketrans({"'":None}))
+        # google_list = google_output.split(",")
+        print(google_output)
+
+        # Format the google body for ROWS
+        body = {
+            'majorDimension': 'ROWS',
+            'values': [
+                google_output,
+            ],
+            }
+        #Print the result to google sheets with append enabled
+        # result = sheet.values().append(
+        #     spreadsheetId=SPREADSHEET_ID, range=STATS_TABLE_WRITE,
+        #     valueInputOption='USER_ENTERED', body=body).execute()
         # Return Team A and Team B to the results template
         return render_template('post.html')
     # If request method is not POST then it must be GET
