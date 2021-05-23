c77bf3419c27d6ffa8f477e617a8342b95503a46
Richard Bignell <bignellrp@gmail.com>
2021-05-23 07:55:02 +0100

    Sort stats by date and limit to 10 items

--- routes/stats.py
+++ routes/stats.py
@@ -1,6 +1,7 @@
 from flask import render_template, Blueprint
 from services.getplayers import _make_stats, _make_player_stats, _fetch_player_sheet, _fetch_stats_sheet
 from operator import itemgetter
+from datetime import datetime
 
 stats_blueprint = Blueprint('stats', __name__, template_folder='templates', static_folder='static')
 
@@ -9,14 +10,18 @@ def stats():
     all_stats = _make_stats(_fetch_stats_sheet())
     player_stats = _make_player_stats(_fetch_player_sheet())
     # Grab game stats from google sheets and display them in a stats table
-
     game_stats = []
     for obj in all_stats:
         game_stats.append((obj.date , obj.team_a_result , obj.team_b_result))
+    # Sort table by date
+    game_stats = sorted(game_stats, key=lambda x: datetime.strptime(x[0], '%Y-%m-%d'), reverse=True)
+    # Only show last 10 matches
+    game_stats = game_stats[0:10]
 
     player_game_stats = []
     for obj in player_stats:
         player_game_stats.append((obj.name , int(obj.wins) , int(obj.draws), int(obj.losses), int(obj.total)))
+    # Sort table by 5th column total
     player_game_stats = sorted(player_game_stats,key=itemgetter(4), reverse=True)
 
     return render_template('stats.html', game_stats = game_stats, player_game_stats = player_game_stats)
