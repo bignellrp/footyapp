9ff6329a3b328ea2b7cb3b3b086cd9425841798c
Richard Bignell <bignellrp@gmail.com>
2021-06-02 07:55:50 +0100

    Moved score update into function

--- services/store_results.py
+++ services/store_results.py
@@ -1,26 +1,25 @@
 from services.getplayers import sheet, SPREADSHEET_ID, _make_score, _fetch_stats_sheet
 
 ## Function to get the end row from stats table to append to
+## TODO Can i grab just end_row from getplayers (it will need the plus 1)
 def _get_stats_endrow():
     make_score, end_row = _make_score(_fetch_stats_sheet())
-    this_weeks_teams = []
-    for obj in make_score:
-        this_weeks_teams.append((obj.date))
     end_row = end_row + 1
+    print(end_row)
     return end_row
 
-## Function to get the date from the end row to compare with
-def _get_stats_date():
+def _get_stats():
     make_score, end_row = _make_score(_fetch_stats_sheet())
     this_weeks_teams = []
-    this_weeks_score = []
     for obj in make_score:
-        this_weeks_teams.append((obj.date))
-    for obj in make_score:
-        this_weeks_score.append((obj.scorea))
-    date = this_weeks_teams[-1]
-    dash = this_weeks_score[-1]
-    return date,dash
+        this_weeks_teams.append((obj.date , obj.scorea, obj.teama_1 , obj.teama_2, obj.teama_3 , obj.teama_4, obj.teama_5 , obj.teamb_1, obj.teamb_2, obj.teamb_3 , obj.teamb_4, obj.teamb_5))
+    team_var = this_weeks_teams[-1]
+    date = team_var[0]
+    #Dash is used in if statement. E.g. Only update if score is not already entered.
+    dash = team_var[1]
+    teama = team_var[2],team_var[3],team_var[4],team_var[5],team_var[6]
+    teamb = team_var[7],team_var[8],team_var[9],team_var[10],team_var[11]
+    return date,dash,teama,teamb
 
 ## Function to update the result using the UPDATE RANGE and the body from the results page
 def _update_result(body):
@@ -34,4 +33,11 @@ def _append_result(body):
     APPEND_RANGE = 'Results!A1:AA1000'
     print(APPEND_RANGE)
     return sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=APPEND_RANGE,
+            valueInputOption='USER_ENTERED', body=body).execute()
+
+## Function to update the result using the SCORE RANGE and the body from the results page
+def _update_score_result(body):
+    SCORE_RANGE = 'Results!B'+str(_get_stats_endrow())
+    print(SCORE_RANGE)
+    return sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=SCORE_RANGE,
             valueInputOption='USER_ENTERED', body=body).execute()
