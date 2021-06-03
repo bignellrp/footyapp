49ce34bf9c552af151a39aa847a83f641bc0d56c
Richard Bignell <bignellrp@gmail.com>
2021-06-03 15:49:28 +0100

    Updated readme with googleapi youtube guide

--- services/store_results.py
+++ services/store_results.py
@@ -6,16 +6,14 @@ def _update_result(body):
     UPDATE_RANGE = 'Results!A'+str(end_row)
     print(UPDATE_RANGE)
     return sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=UPDATE_RANGE,
-            valueInputOption='USER_ENTERED', body=body)
-            #.execute()
+            valueInputOption='USER_ENTERED', body=body).execute()
 
 ## Function to append the result using the APPEND RANGE and the body from the results page
 def _append_result(body):
     APPEND_RANGE = 'Results!A1:AA1000'
     print(APPEND_RANGE)
     return sheet.values().append(spreadsheetId=SPREADSHEET_ID, range=APPEND_RANGE,
-            valueInputOption='USER_ENTERED', body=body)
-            #.execute()
+            valueInputOption='USER_ENTERED', body=body).execute()
 
 ## Function to update the result using the SCORE RANGE and the body from the results page
 def _update_score_result(body):
@@ -23,5 +21,4 @@ def _update_score_result(body):
     SCORE_RANGE = 'Results!B'+str(end_row)
     print(SCORE_RANGE)
     return sheet.values().update(spreadsheetId=SPREADSHEET_ID, range=SCORE_RANGE,
-            valueInputOption='USER_ENTERED', body=body)
-            #.execute()
+            valueInputOption='USER_ENTERED', body=body).execute()
