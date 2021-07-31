eacccf1d91984a836465f7fbdea55f906d64f5cb
Richard Bignell <bignellrp@gmail.com>
2021-07-31 15:03:41 +0100

    Test running on port 80

--- main.py
+++ main.py
@@ -61,7 +61,8 @@ for file in os.listdir("cogs"):
 
 
 # Make a partial app.run to pass args/kwargs to it
-partial_run = partial(app.run, host="127.0.0.1", port=5000, debug=True, use_reloader=False)
+partial_run = partial(app.run, host="0.0.0.0", port=80, debug=False, use_reloader=False)
+#partial_run = bot.run(token)
 
 # Run the Flask app in another thread.
 # Unfortunately this means we can't have hot reload
@@ -73,3 +74,6 @@ t.start()
 
 # Run the bot
 bot.run(token)
+
+#if __name__ == "__main__":
+#    app.run(host="127.0.0.1", debug=False, port=5000)
