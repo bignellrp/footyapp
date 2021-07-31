30a5ff37b5007a3866fb9baafca8376f1ba3978f
Richard Bignell <bignellrp@gmail.com>
2021-07-31 19:34:55 +0100

    Message starts with thumbs up

--- cogs/messages.py
+++ cogs/messages.py
@@ -11,13 +11,13 @@ class Messages(commands.Cog):
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
-        if message.content.startswith('in'):
+        if message.content.startswith('👍'):
             print("Player is in:", message.author.name)
             _update_playing_status(message.author.name)
             _,_,_,_,player_count = _get_players_table(_fetch_players_table())
             msg = 'You are on the team {0.author.mention}. There are {1} places remaining'.format(message,player_count)
             await message.channel.send(msg)
-        if message.content.startswith('out'):
+        if message.content.startswith('👎'):
             print("Player is out:", message.author.name)
             _modify_playing_status(message.author.name)
             msg = 'Ok, hopefully see you next week {0.author.mention}'.format(message)
