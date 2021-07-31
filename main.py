f3d572d52a7c802d09d6ac975a902123a6a3080c
Richard Bignell <bignellrp@gmail.com>
2021-07-31 12:21:33 +0100

    Rollback cert fix

--- main.py
+++ main.py
@@ -1,44 +1,75 @@
-# install_certifi.py
-#
-# sample script to install or update a set of default Root Certificates
-# for the ssl module.  Uses the certificates provided by the certifi package:
-#       https://pypi.python.org/pypi/certifi
-
+##https://flask-discord.readthedocs.io/en/latest/introduction.html
+##https://gist.github.com/Peppermint777/c8465f9ce8b579a8ca3e78845309b832
 import os
-import os.path
-import ssl
-import stat
-import subprocess
-import sys
+from flask import Flask, url_for, redirect
+from flask_discord import DiscordOAuth2Session
+from routes.compare import compare_blueprint
+from routes.index import index_blueprint
+from routes.leaderboard import leaderboard_blueprint
+from routes.stats import stats_blueprint
+from routes.result import result_blueprint
+from routes.score import score_blueprint
+from threading import Thread
+from functools import partial
+import discord
+from discord.ext import commands
+import json
 
-STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
-             | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
-             | stat.S_IROTH |                stat.S_IXOTH )
+# Initialise our app and the bot itself
+# https://discordpy.readthedocs.io/en/latest/intents.html
+#intents = discord.Intents(members=True)
+#intents.members = True
+#bot = commands.Bot(command_prefix="$", intents=intents)
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+app = Flask(__name__)
+
+##Register the blueprint for each route
+app.register_blueprint(index_blueprint)
+app.register_blueprint(compare_blueprint)
+app.register_blueprint(leaderboard_blueprint)
+app.register_blueprint(stats_blueprint)
+app.register_blueprint(result_blueprint)
+app.register_blueprint(score_blueprint)
+
+##Flask session needs a key.
+##https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-files
+app.config.from_pyfile('config.py')
+app.config['SESSION_TYPE'] = 'filesystem'
+
+##Get keys from token json
+path_to_token = "./services/tokens.json"
+with open(path_to_token, "r") as handler:
+    info = json.load(handler)
+
+app.secret_key = info["session"]
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "true"
+app.config["DISCORD_CLIENT_ID"] = info["CLIENT_ID"]
+app.config["DISCORD_CLIENT_SECRET"] = info["CLIENT_SECRET"]
+app.config["DISCORD_REDIRECT_URI"] = info["RI"]
+app.config["DISCORD_BOT_TOKEN"] = info["discord_token"]
+token = info["discord_token"]
+
+##Register Cogs with Discord
+discord = DiscordOAuth2Session(app)
+
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        bot.load_extension(f"cogs.{name}")
 
 
-def main():
-    openssl_dir, openssl_cafile = os.path.split(
-        ssl.get_default_verify_paths().openssl_cafile)
+# Make a partial app.run to pass args/kwargs to it
+partial_run = partial(app.run, host="127.0.0.1", port=5000, debug=True, use_reloader=False)
 
-    print(" -- pip install --upgrade certifi")
-    subprocess.check_call([sys.executable,
-        "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
+# Run the Flask app in another thread.
+# Unfortunately this means we can't have hot reload
+# (We turned it off above)
+# Because there's no signal support.
 
-    import certifi
+t = Thread(target=partial_run)
+t.start()
 
-    # change working directory to the default SSL directory
-    os.chdir(openssl_dir)
-    relpath_to_certifi_cafile = os.path.relpath(certifi.where())
-    print(" -- removing any existing file or link")
-    try:
-        os.remove(openssl_cafile)
-    except FileNotFoundError:
-        pass
-    print(" -- creating symlink to certifi certificate bundle")
-    os.symlink(relpath_to_certifi_cafile, openssl_cafile)
-    print(" -- setting permissions")
-    os.chmod(openssl_cafile, STAT_0o775)
-    print(" -- update complete")
-
-if __name__ == '__main__':
-    main()
+# Run the bot
+bot.run(token)
