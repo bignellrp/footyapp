15f2d3cea70923871a2e112ada7c229ccfec173b
Richard Bignell <bignellrp@gmail.com>
2021-08-25 18:13:30 +0100

    Fixed threading for discord bot

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 import discord
 from discord.ext import commands
+from threading import Thread
+import asyncio
 import json
 
-# Initialise our app and the bot itself
-# https://discordpy.readthedocs.io/en/latest/intents.html
+##Initialise our app and the bot itself
+##https://discordpy.readthedocs.io/en/latest/intents.html
+
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -21,7 +24,22 @@ for file in os.listdir("cogs"):
         name = file[:-3]
         bot.load_extension(f"cogs.{name}")
 
-#bot.run(token) #If enabled then it starts first and webserver does not start
+##Class for Running Discord in a thread
+##https://www.reddit.com/r/Discord_Bots/comments/pa57zv/discord_py_flask_uwsgi/
+##
+##ValueError: set_wakeup_fd only works in main thread
+##https://github.com/Rapptz/discord.py/issues/1598
 
-# if __name__ == "__main__":
-#     bot.run(token)
+class async_discord_thread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.loop = asyncio.get_event_loop()
+        self.start()
+
+    async def starter(self):
+        await bot.start(token) #Changed to start from run
+
+    def run(self):
+        self.name = 'Discord.py'
+        self.loop.create_task(self.starter())
+        self.loop.run_forever()
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from routes.leaderboard import leaderboard_blueprint
 from routes.stats import stats_blueprint
 from routes.result import result_blueprint
 from routes.score import score_blueprint
-from threading import Thread
 import bot
 import json
 
@@ -25,18 +24,6 @@ with open(path_to_token, "r") as handler:
     info = json.load(handler)
 app.secret_key = info["session"]
 
-def run():
-#   #app.run(host="127.0.0.1", debug=False, port=5000)
-   app.run(host="0.0.0.0", port=5000, debug=False, use_reloader=False)
-#   #bot.run(token) #ValueError: set_wakeup_fd only works in main thread
-
-t = Thread(target=run)
-t.start()
-bot.bot.run(bot.token)
-
 if __name__ == "__main__":
-    #app.run(host="127.0.0.1", debug=False, port=5000, threaded=True)
-    t = Thread(target=run)
-    t.start()
-    # #app.run(host="127.0.0.1", debug=False, port=5000)
-    bot.bot.run(bot.token)
+    bot.async_discord_thread() #Run Discord Bot on a thread
+    app.run(host="127.0.0.1", debug=False, port=5000)
