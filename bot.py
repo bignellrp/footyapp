6e0229f0f09f9f903d6cb0045fd68d2dfdef9b84
Richard Bignell <bignellrp@gmail.com>
2021-08-26 21:14:06 +0100

    Added wipe tally to cron

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import asyncio
 import json
 from services.get_oscommand import GITBRANCH, IFBRANCH
 import aiocron
+from services.post_spread import wipe_tally
 
 ##Initialise our app and the bot itself
 ##https://discordpy.readthedocs.io/en/latest/intents.html
@@ -50,6 +51,7 @@ class async_discord_thread(Thread):
 @aiocron.crontab('0 6 * * SUN')
 async def cronmsg():
     channel = bot.get_channel(CHANNEL_ID)
+    wipe_tally()
     await channel.send('Whos available to play this week?')
 
 if  IFBRANCH in GITBRANCH: #Equals not working for some reason
