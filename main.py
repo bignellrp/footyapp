50fdaf42ecb3cbd677c26d649fbb7fd1d5ef871e
Richard Bignell <bignellrp@users.noreply.github.com>
2021-08-27 19:52:40 +0100

    convert blueprint register to loop

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from flask import Flask
-from routes.compare import compare_blueprint
-from routes.index import index_blueprint
-from routes.leaderboard import leaderboard_blueprint
-from routes.stats import stats_blueprint
-from routes.result import result_blueprint
-from routes.score import score_blueprint
+import os
+#from routes.compare import compare_blueprint
+#from routes.index import index_blueprint
+#from routes.leaderboard import leaderboard_blueprint
+#from routes.stats import stats_blueprint
+#from routes.result import result_blueprint
+#from routes.score import score_blueprint
 import bot
 import json
 
 app = Flask(__name__)
 
 ##Register the blueprint for each route
-app.register_blueprint(index_blueprint)
-app.register_blueprint(compare_blueprint)
-app.register_blueprint(leaderboard_blueprint)
-app.register_blueprint(stats_blueprint)
-app.register_blueprint(result_blueprint)
-app.register_blueprint(score_blueprint)
+
+for file in os.listdir("routes"):
+    if file.endswith(".py"):
+        name = file[:-3]
+        from routes.{name} import {name}_blueprint
+        app.register_blueprint({name}_blueprint)
+
+#app.register_blueprint(index_blueprint)
+#app.register_blueprint(compare_blueprint)
+#app.register_blueprint(leaderboard_blueprint)
+#app.register_blueprint(stats_blueprint)
+#app.register_blueprint(result_blueprint)
+#app.register_blueprint(score_blueprint)
 
 ##Get keys from token json
 path_to_token = "./services/tokens.json"
