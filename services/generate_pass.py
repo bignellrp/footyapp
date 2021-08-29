110d6a153588ff89f52c9dfd1c268bd4e24231c2
Richard Bignell <bignellrp@gmail.com>
2021-08-29 18:48:11 +0100

    Removed python data and updated readme

--- services/generate_pass.py
+++ services/generate_pass.py
@@ -1,2 +1,15 @@
 import os
-print('SECRET_KEY =', os.urandom(24).hex())
+print('{\n'
+    +'"session": "'
+    +os.urandom(24).hex()
+    +'",\n'
+    +'"slack_token": "",\n'
+    +'"discord_token": "",\n'
+    +'"discord_token_dev": "",\n'
+    +'"discord_webhook": "",\n'
+    +'"discord_webhook_dev": "",\n'
+    +'"SPREADSHEET_ID": "",\n'
+    +'"channel_id": "",\n'
+    +'"channel_id_dev": ""\n'
+    +'}'
+    )
